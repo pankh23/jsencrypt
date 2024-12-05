5410d3aa134a87413bc2eb4f45588219a2ca87ec
JiyanshKalra <jiyanshkalra@icloud.com>
2024-12-05 11:40:58 +0530

    vsdf

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ key = load_key()
 
 # Start a TCP server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("192.168.56.1", 12345))  # Listen on all network interfaces (0.0.0.0) and port 12345
+server_socket.bind(("0.0.0.0", 12345))  # Listen on all network interfaces (0.0.0.0) and port 12345
 server_socket.listen(1)
 print("Server listening on port 12345...")
 
